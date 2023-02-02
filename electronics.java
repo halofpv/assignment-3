72a41ded72fbef8f2e119dcba119685199f31f00
halofpv <124281806+halofpv@users.noreply.github.com>
2023-02-02 21:59:37 +0600

    Create electronics.java

---
+++ electronics.java
@@ -0,0 +1,89 @@
+abstract class electronicsAbstract extends main//шаблон для класса
+{
+    public abstract String wireInf();
+    public abstract String microshemeInf();
+    public abstract String batteryInf();
+}
+
+public final class electronics extends electronicsAbstract//класс категории
+{
+    public static String wire = "Fat wire";
+    public static String microsheme = "Video board";
+    public static String battery = "16.8V 100A";
+    public static double wirePrice = rEP.rand();
+    public static double microshemePrice = rEP.rand();
+    public static double batteryPrice = rEP.rand();
+
+    @Override
+    public String wireInf()
+    {
+        String uwq = wire + ": " + wirePrice;
+        return uwq;
+    }
+
+    @Override
+    public String microshemeInf()
+    {
+        String uwq = microsheme + ": " + microshemePrice;
+        return uwq;
+    }
+
+    @Override
+    public String batteryInf()
+    {
+        String uwq = battery + ": " + batteryPrice;
+        return uwq;
+    }
+
+    public void Electro(int user)
+    {
+        for(int e = 0; e < 4; e++)
+        {
+            System.out.println("Find your electronic component:");
+            String va = sc.next();
+            if (va.equals("wire") || va.equals("Wire")) {
+                System.out.println(wireInf());
+                out.condition();
+                String te = sc.next();
+                if(te.equals("Yes") || te.equals("yes"))
+                {
+                    m.money(user, 1,3);
+                }
+                Electro(user);
+            }
+            else if (va.equals("microsheme") || va.equals("Microsheme"))
+            {
+                System.out.println(microshemeInf());
+                out.condition();
+                String te = sc.next();
+                if(te.equals("Yes") || te.equals("yes"))
+                {
+                    m.money(user, 2, 3);
+                }
+                Electro(user);
+            }
+            else if (va.equals("battery") || va.equals("Battery"))
+            {
+                System.out.println(batteryInf());
+                out.condition();
+                String te = sc.next();
+                if(te.equals("Yes") || te.equals("yes"))
+                {
+                    m.money(user, 3, 3);
+                }
+                Electro(user);
+            }
+            else if (va.equals("exit") || va.equals("Exit"))
+            {
+                Cabinet(user);
+                break;
+            }
+            else
+            {
+                System.out.println("Nothing has been founded");
+                Electro(user);
+                break;
+            }
+        }
+    }
+}
