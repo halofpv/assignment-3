4f2c17339d461dfc1317de0c20912ebaec3c9454
halofpv <124281806+halofpv@users.noreply.github.com>
2023-02-02 22:00:27 +0600

    Create out.java

---
+++ out.java
@@ -0,0 +1,26 @@
+public interface out
+{
+    static void waiting()
+    {
+        System.out.println("Transaction in process...");
+        System.out.print("Thank you! Wait a delivery. Your post code is: ");
+    }
+    static void condition()
+    {
+        System.out.println("Do you want buy this?");
+        System.out.println(">Yes");
+        System.out.println(">No");
+    }
+    static void cancel()
+    {
+        System.out.println("Purchase has been declined");
+    }
+    static void car()
+    {
+        System.out.println("Find your car below: ");
+    }
+    static void errorM()
+    {
+        System.out.println("Transaction error(30:03): not enough money");
+    }
+}
