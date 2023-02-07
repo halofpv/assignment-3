29d5be53f6b3607d707f5e0d80be9c306dd89a17
halofpv <124281806+halofpv@users.noreply.github.com>
2023-02-07 10:20:08 +0600

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -9,56 +13,29 @@ public class main
         String a = sc.next();
         outMenu.lmn();
         int b = sc.nextInt();
-        if(a.equals(cm[0].mail) && (b == cm[0].password))
+        for(int i = 0; i < 4; i++)
         {
-            outMenu.ac();
-            Cabinet(0);
+            if(a.equals(cm[i].mail) && (b == cm[i].password))
+            {
+                outMenu.ac();
+                Cabinet(i);
+            }
         }
-        else if(a.equals(cm[1].mail) && (b == cm[1].password))
-        {
-            outMenu.ac();
-            Cabinet(1);
-        }
-        else if(a.equals(cm[2].mail) && (b == cm[2].password))
-        {
-            outMenu.ac();
-            Cabinet(2);
-        }
-        else if(a.equals(cm[3].mail) && (b == cm[3].password))
-        {
-            outMenu.ac();
-            Cabinet(3);
-        }
-        else
-        {
             outMenu.error();
             logIn();
-        }
     }
     public static void Cabinet(int i)
     {
-        if (i == 0)
+        for(int j= 0; j < 4; j++)
         {
-            System.out.println(cm[0].getter2());
+            if(j==i)
+            {
+                System.out.println(cm[i].getter2());
+            }
         }
-        else if (i == 1)
-        {
-            System.out.println(cm[1].getter2());
-        }
-        else if (i == 2)
-        {
-            System.out.println(cm[2].getter2());
-        }
-        else if (i == 3)
-        {
-            System.out.println(cm[3].getter2());
-        }
-
         outMenu.menu();
         System.out.println(cm[i].getter2());
         String menuWord = sc.next();
-        for (int j = 0; j < 4; j++)
-        {
             if (menuWord.equals("Electronics") || menuWord.equals("electronics")) {
                 e.Electro(i);
                 Cabinet(i);
@@ -78,37 +55,63 @@ public class main
                 outMenu.errorMenu();
                 logIn();
             }
-        }
     }
-        static consumer[] cm = new consumer[5];
-        static Scanner sc = new Scanner(System.in);
+    static consumer[] cm = new consumer[5];
+    static Scanner sc = new Scanner(System.in);
 
-        static randPassword rPss = new randPassword();
-        static randID rID = new randID();
-        static randBank rM = new randBank();
-        static randCarPrice rCrP = new randCarPrice();
-        static randClosesPrice rClP = new randClosesPrice();
-        static randHomePrice rHP = new randHomePrice();
-        static randElectronicsPrice rEP = new randElectronicsPrice();
+    static randPassword rPss = new randPassword();
+    static randID rID = new randID();
+    static randBank rM = new randBank();
+    static randCarPrice rCrP = new randCarPrice();
+    static randClosesPrice rClP = new randClosesPrice();
+    static randHomePrice rHP = new randHomePrice();
+    static randElectronicsPrice rEP = new randElectronicsPrice();
 
-        static Transaction m = new Transaction();
-        static electronics e = new electronics();
-        static home h = new home();
-        static closes c = new closes();
-        static cars cr = new cars();
+    static Transaction m = new Transaction();
+    static electronics e = new electronics();
+    static home h = new home();
+    static closes c = new closes();
+    static cars cr = new cars();
 
-        public static void main(String[] args)
-        {
-            cm[0] = new consumer(rID.rand(), "Roman", "roman@mail", rPss.rand(), rM.rand());
-            cm[1] = new consumer(rID.rand(), "Khaknazar", "khaknazar@mail", rPss.rand(), rM.rand());
-            cm[2] = new consumer(rID.rand(), "Erlen", "erlen@mail", rPss.rand(), rM.rand());
-            cm[3] = new consumer(rID.rand(), "Nurasyl", "nurasyl@mail", rPss.rand(), rM.rand());
-            ArrayList<String> list = new ArrayList<String>();
-            list.add(cm[0].getter());
-            list.add(cm[1].getter());
-            list.add(cm[2].getter());
-            list.add(cm[3].getter());
-            System.out.println(list);
-            logIn();
+    public static void main(String[] args)
+    {
+        String connectionUrl = "jdbc:postgresql://localhost:5432/postgres";
+
+        Connection con = null;
+        ResultSet rs = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(connectionUrl, "postgres", "12345");
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("select * from example");
+
+            while (rs.next())
+                System.out.println(rs.getString("id") + "  "
+                        + rs.getString("mail") + "  " + rs.getString("password"));
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                System.out.println("Exception occurred while closing the connection: " + e.getMessage());
+            }
         }
+        cm[0] = new consumer(rID.rand(), "Roman", "roman@mail", rPss.rand(), rM.rand());
+        cm[1] = new consumer(rID.rand(), "Khaknazar", "khaknazar@mail", rPss.rand(), rM.rand());
+        cm[2] = new consumer(rID.rand(), "Erlen", "erlen@mail", rPss.rand(), rM.rand());
+        cm[3] = new consumer(rID.rand(), "Nurasyl", "nurasyl@mail", rPss.rand(), rM.rand());
+        ArrayList<String> list = new ArrayList<String>();
+        list.add(cm[0].getter());
+        list.add(cm[1].getter());
+        list.add(cm[2].getter());
+        list.add(cm[3].getter());
+        System.out.println(list);
+        logIn();
+    }
 }
+
