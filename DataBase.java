59317e9be4663ec7e0fea16699b39722ab0a85bc
halofpv <124281806+halofpv@users.noreply.github.com>
2023-02-17 18:17:59 +0600

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -1,7 +1,8 @@
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DataBase extends Superclass
 {
@@ -16,6 +17,7 @@ public class DataBase extends Superclass
             Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection(connectionUrl, "postgres", "12345");
             stmt = con.createStatement();
+
             rs = stmt.executeQuery("select * from example");
 
             while (rs.next())
@@ -34,4 +36,52 @@ public class DataBase extends Superclass
             }
         }
     }
+    public void changeinfo(String name) throws SQLException
+    {
+        final String DB_USERNAME = "postgres"; //З
+        final String DB_PASSWORD = "12345";
+        final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        String sql = "insert into example (id, mail, password) values (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, rand.rID.toString());
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, rand.rPss.toString());
+        preparedStatement.executeUpdate();
+        System.out.println("U have registered");
+    }
+}
+class Cllient {
+    private String name;
+
+    public void Cllient(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String getName) {
+        this.name = name;
+    }
+    public void changeinfo(String name) throws SQLException
+    {
+        final String DB_USERNAME = "postgres"; //З
+        final String DB_PASSWORD = "12345";
+        final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        String sql = "insert into example (id, mail, password) values (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        //for(int i = 0; i < 4; i++)
+        //{
+        preparedStatement.setString(1, rand.rID.toString());
+        preparedStatement.setString(2, name);
+        preparedStatement.setString(3, rand.rPss.toString());
+        //}
+        preparedStatement.executeUpdate();
+        System.out.println("U have registered");
+    }
+
 }
