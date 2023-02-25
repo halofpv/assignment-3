2478eebe45659036e031defa9af19c1322c3fb84
halofpv <124281806+halofpv@users.noreply.github.com>
2023-02-25 19:18:05 +0600

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -36,7 +36,7 @@ public class DataBase extends Superclass
             }
         }
     }
-    public void changeinfo(String name) throws SQLException
+    public static void changeinfo(String id, String name, String password) throws SQLException
     {
         final String DB_USERNAME = "postgres"; //З
         final String DB_PASSWORD = "12345";
@@ -45,11 +45,60 @@ public class DataBase extends Superclass
         String sql = "insert into example (id, mail, password) values (?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-            preparedStatement.setString(1, rand.rID.toString());
-            preparedStatement.setString(2, name);
-            preparedStatement.setString(3, rand.rPss.toString());
-        preparedStatement.executeUpdate();
-        System.out.println("U have registered");
+                preparedStatement.setString(1, String.valueOf(id));
+                preparedStatement.setString(2, name);
+                preparedStatement.setString(3, String.valueOf(password));
+
+                preparedStatement.executeUpdate();
+    }
+    public static void updateuser(int id, String name, int password, int n) throws SQLException
+    {
+        final String DB_USERNAME = "postgres";
+        final String DB_PASSWORD = "12345";
+        final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        Statement statement = connection.createStatement();
+        String SQL_SELECT_PERSON = "select * from example";
+        ResultSet result = statement.executeQuery(SQL_SELECT_PERSON);
+        while (result.next())
+        {
+            String login = result.getString("id");
+            String pass = result.getString("mail");
+            String id1 = result.getString("password");
+                String updateSql = "UPDATE example SET id = ?, mail = ?, password = ?";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(updateSql))
+                {
+                    preparedStatement.setString(1, String.valueOf(cm[n].ID));
+                    preparedStatement.setString(2, name);
+                    preparedStatement.setString(3, String.valueOf(cm[n].password));
+                    preparedStatement.executeUpdate();
+                }
+
+        }
+    }
+    public static void deleteuser(int id, String name, int password) throws SQLException {
+        final String DB_USERNAME = "postgres";
+        final String DB_PASSWORD = "12345";
+        final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        Statement statement = connection.createStatement();
+        String SQL_SELECT_PERSON = "select * from example";
+        ResultSet result = statement.executeQuery(SQL_SELECT_PERSON);
+
+        while (result.next())
+        {
+                String id1 =result.getString("id");
+
+                String deleteSql = "delete from example where id = ? and mail = ? and password = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(deleteSql);
+            preparedStatement.setString(1, "111");
+            preparedStatement.setString(2, "111");
+            preparedStatement.setString(3, "111");
+            preparedStatement.executeUpdate();
+
+            preparedStatement.executeUpdate();
+                System.out.println("The User has deleted.");
+        }
     }
 }
 class Cllient {
@@ -66,7 +115,7 @@ class Cllient {
     public void setName(String getName) {
         this.name = name;
     }
-    public void changeinfo(String name) throws SQLException
+    public void changeinfo(int ID, String name, int password) throws SQLException
     {
         final String DB_USERNAME = "postgres"; //З
         final String DB_PASSWORD = "12345";
@@ -74,14 +123,11 @@ class Cllient {
         Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
         String sql = "insert into example (id, mail, password) values (?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        //for(int i = 0; i < 4; i++)
-        //{
+
         preparedStatement.setString(1, rand.rID.toString());
         preparedStatement.setString(2, name);
         preparedStatement.setString(3, rand.rPss.toString());
-        //}
-        preparedStatement.executeUpdate();
-        System.out.println("U have registered");
-    }
 
+        preparedStatement.executeUpdate();
+    }
 }
